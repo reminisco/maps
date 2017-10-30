e7c3bc3550e6bd4a382f0dbc8fdc60543c40ce9a
warrick hill <github@hikingduck.co.uk>
2017-10-30 23:29:29 +0000

    css bug fixes

--- client/src/Content/Components/FilteredMapTiles.jsx
+++ client/src/Content/Components/FilteredMapTiles.jsx
@@ -1,40 +1,21 @@
 import React from 'react';
-import Card, {CardActions, CardContent, CardMedia} from 'material-ui/Card';
-import {GridList, GridListTile, GridListTileBar} from 'material-ui/GridList';
-import Typography from 'material-ui/Typography';
+import MapTile from './MapTile';
 import {withStyles} from 'material-ui/styles';
 
 const styles = theme => ({
-    container: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        background: theme.palette.background.paper,
-    },
-    gridList: {
-        width: "100%",
-    },
+    container: {}
 });
 
-const MapTile = (props) => {
+const FilteredMapTiles = (props) => {
 
     const {classes, maps} = props;
     return (
         <div className={classes.container}>
-            <GridList cols="3" cellHeight={250} className={classes.gridList} spacing={25}>
-                {maps.map(map => (
-                    <GridListTile key={map.id}>
-                        <img src={"https://bosmaps.hikingduck.co.uk/img/maps/" + map.id + ".jpg"} alt={map.name}/>
-                        <GridListTileBar
-                            title={map.number}
-                            subtitle={<Typography type="title" color="inherit">{map.name}</Typography>}
-                        />
-                    </GridListTile>
-                ))}
-            </GridList>
+            {maps.map(map => (
+                <MapTile {...map}/>
+            ))}
         </div>
     );
 };
 
-export default withStyles(styles)(MapTile);
+export default withStyles(styles)(FilteredMapTiles);
